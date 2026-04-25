750e045a066b8eebc761aa5facf339ae90c7cb51
Anand.s <anandkiccha45@gamil.com>
2026-04-25 20:25:34 +0530

    Adding all files

---
+++ urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    
+    path('blogs/', views.blog_page, name='blog_page'),
+    path('create/',views.create_post,name='create_post'),
+    path('delete/<int:id>/',views.delete_blog,name='delete_blog'),
+    path('edit/<int:id>/',views.edit_blog,name='edit_blog'),
+    path('reg/',views.reg_page,name='reg_page'),
+    path('log/',views.log_page,name='log_page'),
+    path('logot/',views.logout_page,name='logout_page'),
+]
---
+++ views.py
@@ -0,0 +1,105 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import *
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+@login_required(login_url='/log/')
+def create_post(request):
+    if request.method=='POST':
+        data=request.POST
+        image=request.FILES.get("image")
+        post_title=data.get('post_title')
+        description=data.get('description')
+
+        Blog.objects.create(
+            image=image,
+            post_title=post_title,
+            description=description
+        )
+        return redirect('/create/')
+    print("User:", request.user)
+    print("Authenticated:", request.user.is_authenticated)
+    return render(request,'temp/create.html')
+@login_required(login_url='/log/')
+def blog_page(request):
+    queryset=Blog.objects.all()
+    search=request.GET.get('search')
+    if search:
+        queryset=queryset.filter(post_title__icontains=search)
+    context={'blogs':queryset}
+    return render(request,'temp/blog.html',context)
+
+@login_required(login_url='/log/')
+def delete_blog(request,id):
+    blog=Blog.objects.get(id=id)
+    blog.delete()
+    return redirect('/blogs/')
+
+@login_required(login_url='/log/')
+def edit_blog(request,id):
+    blog=Blog.objects.get(id=id)
+    context={'blogs':blog}
+    if request.method=='POST':
+        data=request.POST
+        image=request.FILES.get("image")
+        post_title=data.get('post_title')
+        description=data.get('description')
+
+        blog.post_title=post_title
+        blog.description=description
+        if image:
+            blog.image=image
+        blog.save()
+
+        return redirect('/blogs/')
+    return render(request,'temp/Edit.html',context)
+
+def reg_page(request):
+    if request.method=="POST":
+        first_name=request.POST.get("first_name")
+        last_name=request.POST.get("last_name")
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+
+        user=User.objects.filter(username=username)
+        if user.exists():
+            messages.error(request,"User already exist")
+            return redirect('/reg/')
+        user=User.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            username=username
+        )
+        user.set_password(password)
+        user.save()
+        messages.info(request,'Account created successfully')
+        return redirect('/reg/')
+    return render(request,'temp/reg.html')
+
+
+def log_page(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        if not User.objects.filter(username=username).exists():
+            messages.error(request,'Invalid username')
+            return redirect('/log/')
+        user=authenticate(username=username,password=password)
+        if user is None:
+            messages.error(request,'Invalid password')
+            return redirect('/log/')
+        else:
+            login(request,user)
+            return redirect('/blogs/')
+    return render(request,'temp/log.html')
+
+def logout_page(request):
+    logout(request)
+    return redirect('/blogs/')
+
+    
